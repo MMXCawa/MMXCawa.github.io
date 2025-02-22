e6ba2a6c98c35402489aa24038af53b091c56612
Nature_9 自然N9 <133771252+Mxher07@users.noreply.github.com>
2025-02-23 00:31:02 +0800

    Add files via upload

---
+++ Games/猜猜border/pre.js
@@ -0,0 +1,175 @@
+const Ad = `
+     冰与火之舞
+     弱拍
+     发条
+     情书
+     午夜列车
+     脉冲
+     旋转至胜
+     密林之城
+     经典追求
+     蝴蝶星球
+     赫拉克勒斯
+     人造战车
+     距离
+     第三波反转
+     制作组
+     最后的希望
+     玫瑰花园
+     恐惧孽生
+     海王星天外体
+     Miko Skip
+     精灵派对
+     感谢你玩我们的游戏
+     一个被遗忘的夜晚
+     选项
+     It Go
+     Night Wander (cnsouka remix)
+     La nuit de vif
+     EMOMOMO
+     新生
+     靛蓝之歌
+     这里没有提示
+     第三太阳
+     神圣干涉
+     `;
+     const Ph = `
+     Glaciaxion
+     Eradication Catastrophe
+     Credits
+     Dlyrotz
+     Engine x Start!! (melody mix)
+     光
+     Winter ↑cube↓
+     混乱-Confusion
+     Cipher : /2&//<|0
+     FULi AUTO SHOOTER
+     HumaN
+     [PRAW]
+     Cereris
+     Pixel Rebelz
+     Non-Melodic Ragez(MUG Edit)
+     Sultan Rage
+     Class Memories
+     -SURREALISM-
+     Bonus Time
+     ENERGY SYNERGY MATRIX
+     NYA!!!(Phigros ver.)
+     JunXion Between Life And Death(VIP Mix)
+     cryout
+     Reimei
+     Spasmodic
+     Leave All Behind
+     Colorful Days♪
+     micro.wav
+     重生
+     望影の方舟Six
+     NO ONE YES MAN
+     Igallta
+     Clock Paradox	
+     Chronologika
+     Chronomia
+     Chronos Collapse - La Campanella	
+     Rrhar'il
+     Crave Wave
+     The Chariot ~REVIIVAL~
+     Luminescence
+     Retribution
+     DESTRUCTION 3,2,1
+     Distorted Fate
+     Ποσειδών
+     WATER
+     Miracle Forest (VIP Mix)
+     MOBILYS
+     Lyrith -迷宮リリス-
+     Demiurge
+     Demonkin
+     Re_Nascence (Psystyle Ver.)	
+     Ark
+     After Dawn
+     INFiNiTE ENERZY -Overdoze-
+     Khalid
+     Quantum Hyperspace
+     Freaky Undulations
+     ~Noble Knights of Tune~
+     PANIC PARADISE
+     Re：End of a Dream
+     心の記憶
+     Spotlight
+     NightTheater
+     Ramification
+     Der Richter
+     ATHAZA
+     Another Me
+     mechanted
+     life flashes before weeb eyes
+     Break Through The Barrier
+     Chronostasis
+     Infinity Heaven
+     Disorder
+     CROSS†SOUL
+     GOODTEK
+     GOODBOUNCE
+     GOODWORLD
+     GOODFORTUNE
+     GOODRAGE
+     Initialize
+     桜樹街道
+     Get Ready!!
+     volcanic
+     XING
+     Final Step!
+     Cthugha
+     Time to Night Sky (feat. Lee Yu Jin)
+     HAZARD
+     Another Me
+     Don't Never Around
+     RESSiSTANCE
+     Apocalypse
+     Protoflicker
+     Horizon Blue
+     You are the Miserable
+     Stasis
+     Shadow
+     心之所向
+     inferior
+     盏茗
+     青芽
+     瓷岁
+     Feast远东之宴
+     神話
+     El último baile
+     Trojan
+     Temporal Shifting
+     Ad astra per aspera
+     A journey to the moonlight
+     Journey with You
+     Secret Illumination
+     Inverted World
+     ストレイソウル・アラウンド
+     Manifold Hypothesis
+     K.Moe (VIP)
+     月詠に鳴る
+     TECHNOPOLIS 2085
+     祈 -我ら神祖と共に歩む者なり-
+     Xenolith
+     Artificial Existence
+     Indelible Scar
+     零號車輛
+     幽世桔梗
+     千紫万紅
+     明鏡烈火
+     Cuvism³
+     iL-Artifact
+     a truth seeker -Communication with Utopia will be lost-
+     君往何处 (Quo Vadis)
+     暗夜苏醒 (REANIMATE)
+     黄金之城 (GOLD TOWN)
+     双重间谍 (Double Agent)
+     幻影鬼魅 (PLEASE)
+     S.A.T.E.L.L.I.T.E.
+     Dance with Silence
+     Labyrinth in Kowloon: Walled World	
+     Compute It With Some Devilish Alcoholic Steampunk Engines
+     +ERABY+E CONNEC+10N
+     `;
